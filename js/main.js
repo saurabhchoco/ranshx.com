7a52f9ee8a294e8e6e80802fd403f89cc0b9dce1
Siddhartha Chakraborty <siddharthachakraborty@Siddharthas-MacBook-Air.local>
2026-02-05 15:55:31 +0530

    feature: introduce parallax stack for hero and who we are and hero video

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const menuToggle = document.getElementById("menuToggle");
 const overlayMenu = document.getElementById("overlayMenu");
 const menuClose = document.getElementById("menuClose");
+const hero = document.querySelector('.hero-parallax');
+const parallaxStack = document.querySelector('.parallax-stack');
 
 function closeMenu() {
   overlayMenu.classList.remove("active");
@@ -16,4 +18,25 @@ menuClose.addEventListener("click", closeMenu);
 
 document.querySelectorAll(".overlay-menu a").forEach(link => {
   link.addEventListener("click", closeMenu);
+});
+
+// parallax scroll
+window.addEventListener('scroll', () => {
+  const scrollY = window.scrollY;
+  const heroHeight = hero.offsetHeight;
+
+  let progress = Math.min(scrollY / heroHeight, 1);
+
+  hero.style.opacity = 1 - progress * 0.6;
+  hero.style.transform = `scale(${1 - progress * 0.05})`;
+});
+
+window.addEventListener('scroll', () => {
+  const stackBottom =
+    parallaxStack.offsetTop + parallaxStack.offsetHeight;
+
+  if (window.scrollY > stackBottom) {
+    hero.style.opacity = 1;
+    hero.style.transform = 'none';
+  }
 });
